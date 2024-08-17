b6ebb6f6461888cc21cc7ecba54901cc468c0c40
Mo Li <2568818204@qq.com>
2024-08-17 15:19:43 +0000

    fix issue of docs language link

--- docs/zh_cn/conf.py
+++ docs/zh_cn/conf.py
@@ -59,7 +59,9 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+html_context = {
+    'github_version': 'latest',
+}
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
