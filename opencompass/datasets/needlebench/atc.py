5d2c36978624967ef6331ba2ddbaaf9bfd02d6b2
Mor-Li <2568818204@qq.com>
2025-03-12 05:52:54 +0000

    update needlebench

--- opencompass/datasets/needlebench/atc.py
+++ opencompass/datasets/needlebench/atc.py
@@ -60,6 +60,43 @@ relationship_templates_en = [
     "For {B}, {A} is more than just a {relationship}; {A} is a lifelong mentor of {B}.",
 ]
 
+shuffled_story_with_prompt_zh_CN = """下面是对你的多步推理能力的测试，这个测试叫做祖先追溯测试，我们会模拟不同人的家庭亲属关系，你的任务是在其中不断推理，直到找到最年长的祖先。
+                
+例如：
+例子1.如果张强的父亲是马克，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中张强能够向上追溯到的最年长的亲人就是马克。
+例子2.如果李明的姥姥是张红，而张红的父亲是张强，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中李明能够向上追溯到的最年长的亲人就是张强。
+例子3.如果小明是张红的曾孙女，张红的祖母是王华，王华的父亲是王刚，除此以外提供的文本中没有更多关于亲属关系的信息，那么小明能够向上追溯到的最年长的亲人就是王刚。
+
+注意：
+1. 你不必纠结这个测试中的人名的性别关系，例如，一个通常被视为女性化的名字仍然可以是其他人的父亲，我们的重点是谁更年长。
+2. 忽略这个测试中的姓氏遗传问题，例如，李明仍然可能是王鹏的亲生父亲，我们只关注谁更年长，不必纠结孩子是否应该继承父亲或母亲的性别。
+3. 在回答的最后，将你的答案放在\\boxed{{}}中，例如：“所以{last_person}能向上追溯到的最年长的亲人就是\\boxed{{某人（你的答案）}}”
+
+现在，打乱的家族关系文本如下：
+{shuffled_story}
+
+在上面提供的打乱的家族关系文本中，'{last_person}'的能够向上追溯到的最年长的亲人是谁？
+"""
+
+shuffled_story_with_prompt_en = """Here is a test for multi-step reasoning ability called the Ancestral Trace Challenge. In this test, we will simulate different people's familial relationships, and your task is to continuously reason through them until you identify the eldest ancestor.
+
+For example:
+Example 1: If James Hill's father is Jasmine Lane, and no further information about familial relationships is provided in the text, then the oldest relative James Hill can trace back to in the provided text is \\boxed{{Jasmine Lane}}.
+Example 2: If Andrew Williams's grandmother is Dan Newton, and Dan Newton's father is James Hill, and no further information about familial relationships is provided in the text, then the oldest relative Andrew Williams can trace back to in the provided text is \\boxed{{James Hill}}.
+Example 3: If Jeff White's father is Kevin Le, Dan Newton's grandmother is Jeff White, and Jeff White's father is Kevin Le, and Shelley Mills is Dan Newton's great-granddaughter, and no further information about familial relationships is provided in the text, then the oldest relative Shelley Mills can trace back to in the provided text is \\boxed{{Kevin Le}}.
+
+Notes:
+1. You do not need to worry about the gender consistency of names in this test. For example, a name that is typically considered feminine can still be the father of another person. Our primary focus is on who is older.
+2. Ignore surname inheritance issues. For instance, Andrew Williams could still be the biological father of Christopher Baker. We only care about who is older and do not need to consider whether a child should inherit the father's or mother's surname.
+3. At the end of your response, remember to put your final answer within \\boxed{{}}. For example: "So the oldest relative '{last_person}' can trace back to in the provided text is \\boxed{{somebody (your answer here)}}."
+
+Now, the scrambled family relationships are provided below:
+{shuffled_story}
+
+Given the scrambled family relationships described above, who is the eldest relative that '{last_person}' can trace back to in the context?
+"""
+
+
 @LOAD_DATASET.register_module()
 class NeedleBenchATCDataset(BaseDataset):
 
@@ -126,42 +163,9 @@ class NeedleBenchATCDataset(BaseDataset):
 
             # Generating the prompt based on the language
             if language == 'Chinese':
-                shuffled_story_with_prompt = f"""下面是对你的多步推理能力的测试，这个测试叫做祖先追溯测试，我们会模拟不同人的家庭亲属关系，你的任务是在其中不断推理，直到找到最年长的祖先。
-
-注意：
-1. 你不必纠结这个测试中的人名的性别关系，例如，一个通常被视为女性化的名字仍然可以是其他人的性别，我们的重点是谁更年长。
-2. 忽略这个测试中的姓氏遗传问题，例如，李明仍然可能是王鹏的亲生父亲，我们只关注谁更年长，不必纠结孩子是否应该继承父亲或母亲的性别。
-3. 在回答的最后，用"所以答案是A/B/C/D"的格式回答正确答案(你只能选择一个选项)。
-
-现在，打乱的家族关系文本如下：
-{shuffled_story}
-
-在上面提供的打乱的家族关系文本中，'{last_person}'的能够向上追溯到的最年长的亲人是谁？
-
-例如：
-例子1.如果张强的父亲是马克，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中张强能够向上追溯到的最年长的亲人就是马克。
-例子2.如果李明的姥姥是张红，而张红的父亲是张强，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中李明能够向上追溯到的最年长的亲人就是张强。
-例子3.如果小明是张红的曾孙女，张红的祖母是王华，王华的父亲是王刚，除此以外提供的文本中没有更多关于亲属关系的信息，那么小明能够向上追溯到的最年长的亲人就是王刚。
-"""
+                shuffled_story_with_prompt = shuffled_story_with_prompt_zh_CN.format(shuffled_story=shuffled_story, last_person=last_person)
             elif language == 'English':
-                shuffled_story_with_prompt = f"""Here is a test for multi-step reasoning ability called the Ancestral Trace Challenge. In this test, we will simulate different people's familial relationships, and your task is to continuously reason through them until you identify the eldest ancestor.
-
-Now, the scrambled family relationships are provided below:
-{shuffled_story}
-
-Given the scrambled family relationships described above, who is the eldest relative that '{last_person}' can trace back to in the context?
-
-For example:
-Example 1: If James Hill's father is Jasmine Lane, and no further information about familial relationships is provided in the text, then the oldest relative James Hill can trace back to in the provided text is \\boxed{{Jasmine Lane}}.
-Example 2: If Andrew Williams's grandmother is Dan Newton, and Dan Newton's father is James Hill, and no further information about familial relationships is provided in the text, then the oldest relative Andrew Williams can trace back to in the provided text is \\boxed{{James Hill}}.
-Example 3: If Jeff White's father is Kevin Le, Dan Newton's grandmother is Jeff White, and Jeff White's father is Kevin Le, and Shelley Mills is Dan Newton's great-granddaughter, and no further information about familial relationships is provided in the text, then the oldest relative Shelley Mills can trace back to in the provided text is \\boxed{{Kevin Le}}.
-
-Notes:
-1. You do not need to worry about the gender consistency of names in this test. For example, a name that is typically considered feminine can still be the father of another person. Our primary focus is on who is older.
-2. Ignore surname inheritance issues. For instance, Andrew Williams could still be the biological father of Christopher Baker. We only care about who is older and do not need to consider whether a child should inherit the father's or mother's surname.
-3. At the end of your response, remember to put your final answer within \\boxed{{}}. For example: "So the oldest relative xxx(the person you need to find the eldest relative for) can trace back to in the provided text is \\boxed{{}}."
-
-"""
+                shuffled_story_with_prompt = shuffled_story_with_prompt_en.format(shuffled_story=shuffled_story, last_person=last_person)
             else:
                 prompt = 'Language not supported.'
                 raise Exception('Unsupported language specified. '
@@ -215,7 +219,7 @@ def needlebench_atc_postprocess_v2(text: str) -> str:
     return "None"
 
 
-@ICL_EVALUATORS.register_module()
+@ICL_EVALUATORS.register_module("needlebench_atc_evaluator")
 class NeedleBenchATCEvaluator(BaseEvaluator):
 
     def score(self, predictions, gold):
--- opencompass/datasets/needlebench/multi.py
+++ opencompass/datasets/needlebench/multi.py
@@ -7,9 +7,8 @@ from datasets import Dataset
 from huggingface_hub import hf_hub_download
 
 from opencompass.datasets.base import BaseDataset
-from opencompass.openicl import BaseEvaluator
 from opencompass.registry import LOAD_DATASET
-
+from opencompass.datasets.needlebench.atc import relationship_templates_zh_CN, relationship_terms_zh_CN, relationship_templates_en, relationship_terms_en
 
 def get_random_needles(counter, file_path, num_needles, language):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -21,40 +20,11 @@ def get_random_needles(counter, file_path, num_needles, language):
     names = random.sample(all_names, num_needles)
 
     if language == 'Chinese':
-        relationship_terms = [
-            '父亲', '母亲', '爸爸', '妈妈', '爷爷', '奶奶', '姥姥', '姥爷', '外公', '外婆'
-        ]
-
-        relationship_templates = [
-            '{A}是{B}的{relationship}。',
-            '{B}的{relationship}是{A}。',
-            '{A}作为{B}的{relationship}，对{B}的成长有重要影响。',
-            '{A}不仅是{B}的{relationship}，还是{B}的榜样。',
-            '{B}是{A}所生的孩子。',
-            '{A}对{B}来说，不只是一个{relationship}，还是一个朋友。',
-            '从血缘关系上来看，{A}是{B}的{relationship}。',
-            '{B}知道{A}就是其{relationship}。',
-        ]
+        relationship_terms = relationship_terms_zh_CN
+        relationship_templates = relationship_templates_zh_CN
     elif language == 'English':
-        relationship_terms = [
-            'father', 'mother', 'dad', 'mom', 'grandfather', 'grandmother',
-            'maternal grandmother', 'maternal grandfather',
-            'paternal grandfather', 'paternal grandmother'
-        ]
-
-        relationship_templates = [
-            "{A} is {B}'s {relationship}.",
-            "{B}'s {relationship} is {A}.",
-            ("{A}, as {B}'s {relationship}, "
-             "has a significant impact on {B}'s upbringing."),
-            ("{A} is not only {B}'s {relationship} "
-             "but also {B}'s role model."),
-            '{B} is the child of {A}.',
-            ('For {B}, {A} is not just a {relationship}, '
-             'but also a friend.'),
-            ("{A} is {B}’s {relationship} by blood."),
-            '{B} knows {A} is their {relationship}.',
-        ]
+        relationship_terms = relationship_terms_en
+        relationship_templates = relationship_templates_en
     else:
         raise ValueError(f"Unsupported language '{language}' specified.")
 
@@ -90,7 +60,8 @@ def get_random_needles(counter, file_path, num_needles, language):
     return {
         'needles': family_story_fragments,
         'answer': names[0],
-        'retrieval_question': retrieval_question
+        'retrieval_question': retrieval_question,
+        'last_person': last_person
     }
 
 
@@ -112,7 +83,7 @@ class NeedleBenchMultiDataset(BaseDataset):
         needle_file_name: str,
         num_needles: int,
         diff: int,
-        position: str = 'End',
+        quesiton_position: str = 'End',
     ):
         data = {'prompt': [], 'answer': []}
         tokenizer = tiktoken.encoding_for_model(tokenizer_model)
@@ -154,61 +125,99 @@ class NeedleBenchMultiDataset(BaseDataset):
         def _decode_tokens(tokens):
             return tokenizer.decode(tokens)
 
-        def _modify_retrieval_question(retrieval_question):
-            if language == 'Chinese':
-                guide_retrieval_question = (retrieval_question +
-                                            '在回答之前，请思考文档中与此问题'
-                                            '最相关的内容是什么。')
-                return guide_retrieval_question
-            elif language == 'English':
-                guide_retrieval_question = (
-                    retrieval_question + 'Before answering, please consider'
-                    ' what in the document is most relevant to this question.')
-                return guide_retrieval_question
-            else:
-                raise ValueError(f"Language '{language}' is not supported.")
-
-        def _generate_prompt(context, retrieval_question):
-            if guide:
-                retrieval_question = _modify_retrieval_question(
-                    retrieval_question)
+        def _generate_prompt(context, retrieval_question, last_person):
 
             if language == 'Chinese':
-                if position == 'End':
-                    prompt = ('你是一个善于回答用户问题的智能AI助手\n'
-                              '请保持你的回答简洁清楚。不要说和下面文档中的无关的话'
-                              '，或重复你的回答\n'
-                              f'用户现在给你的文档是{context}\n\n'
-                              f'现在请问：{retrieval_question}')
-                elif position == 'Start':
-                    prompt = ('你是一个善于回答用户问题的智能AI助手\n'
-                              '请保持你的回答简洁清楚。不要说和下面文档中的无关的话'
-                              '，或重复你的回答\n'
-                              f'现在请问：{retrieval_question}',
-                              f'用户现在给你的文档是{context}\n\n')
+                if quesiton_position == 'End':
+                    prompt = f'''这是一个长文本能力的测试，你需要首先阅读下面的长文档，然后根据文档中的信息回答最后的问题。
+长文档的内容如下
+
+<文档>
+{context}
+</文档>
+
+根据文档中的信息，现在请问：{retrieval_question}
+
+例如：
+例子1.如果张强的父亲是马克，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中张强能够向上追溯到的最年长的亲人就是马克。
+例子2.如果李明的姥姥是张红，而张红的父亲是张强，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中李明能够向上追溯到的最年长的亲人就是张强。
+例子3.如果小明是张红的曾孙女，张红的祖母是王华，王华的父亲是王刚，除此以外提供的文本中没有更多关于亲属关系的信息，那么小明能够向上追溯到的最年长的亲人就是王刚。
+
+注意：
+1. 你不必纠结这个测试中的人名的性别关系，例如，一个通常被视为女性化的名字仍然可以是其他人的父亲，我们的重点是谁更年长。
+2. 忽略这个测试中的姓氏遗传问题，例如，李明仍然可能是王鹏的亲生父亲，我们只关注谁更年长，不必纠结孩子是否应该继承父亲或母亲的性别。
+3. 在回答的最后，将你的答案放在\\boxed{{}}中，例如：“所以{last_person}能向上追溯到的最年长的亲人就是\\boxed{{（你的答案）}}”
+
+'''
+                elif quesiton_position == 'Start':
+                    prompt = f'''这是一个长文本能力的测试，你需要首先阅读下面的问题，然后根据最后长文档中的信息回答下面的问题。
+现在请问：{retrieval_question}
+
+例如：
+例子1.如果张强的父亲是马克，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中张强能够向上追溯到的最年长的亲人就是马克。
+例子2.如果李明的姥姥是张红，而张红的父亲是张强，除此以外提供的文本中没有更多关于亲属关系的信息，那么在提供的文本中李明能够向上追溯到的最年长的亲人就是张强。
+例子3.如果小明是张红的曾孙女，张红的祖母是王华，王华的父亲是王刚，除此以外提供的文本中没有更多关于亲属关系的信息，那么小明能够向上追溯到的最年长的亲人就是王刚。
+
+注意：
+1. 你不必纠结这个测试中的人名的性别关系，例如，一个通常被视为女性化的名字仍然可以是其他人的父亲，我们的重点是谁更年长。
+2. 忽略这个测试中的姓氏遗传问题，例如，李明仍然可能是王鹏的亲生父亲，我们只关注谁更年长，不必纠结孩子是否应该继承父亲或母亲的性别。
+3. 在回答的最后，将你的答案放在\\boxed{{}}中，例如：“所以{last_person}能向上追溯到的最年长的亲人就是\\boxed{{（你的答案）}}”
+
+长文档内容的如下
+
+<文档>
+{context}
+</文档>
+
+'''
                 else:
-                    raise ValueError('Unsupported position. '
+                    raise ValueError('Unsupported quesiton_position. '
                                      'Position must be "End" or "Start".')
             elif language == 'English':
-                if position == 'End':
-                    prompt = ('You are an intelligent AI assistant skilled in '
-                              'answering user questions.\n'
-                              'Please keep your answers concise and clear. Do '
-                              'not talk about irrelevant topics or repeat '
-                              'your answers.\nThe document '
-                              f'given to you by the user is {context}\n\n'
-                              f'Now, the question is: {retrieval_question}')
-                elif position == 'Start':
-                    prompt = ('You are an intelligent AI assistant skilled in '
-                              'answering user questions.\n'
-                              'Please keep your answers concise and clear. Do '
-                              'not talk about irrelevant topics or repeat '
-                              'your answers.\n'
-                              f'Now, the question is: {retrieval_question}'
-                              'The document given to you by the user'
-                              f' is {context}\n\n')
+                if quesiton_position == 'End':
+                    prompt = f'''This is a test of long-text capability. You need to first read the long document below, and then answer the final question based on the information in the document.
+The content of the long document is as follows
+
+<Document>
+{context}
+</Document>
+
+Based on the information in the document, now please answer: {retrieval_question}
+
+For example:
+Example 1: If James Hill's father is Jasmine Lane, and no further information about familial relationships is provided in the text, then the oldest relative James Hill can trace back to in the provided text is \\boxed{{Jasmine Lane}}.
+Example 2: If Andrew Williams's grandmother is Dan Newton, and Dan Newton's father is James Hill, and no further information about familial relationships is provided in the text, then the oldest relative Andrew Williams can trace back to in the provided text is \\boxed{{James Hill}}.
+Example 3: If Jeff White's father is Kevin Le, Dan Newton's grandmother is Jeff White, and Jeff White's father is Kevin Le, and Shelley Mills is Dan Newton's great-granddaughter, and no further information about familial relationships is provided in the text, then the oldest relative Shelley Mills can trace back to in the provided text is \\boxed{{Kevin Le}}.
+
+Notes:
+1. You do not need to worry about the gender consistency of names in this test. For example, a name that is typically considered feminine can still be the father of another person. Our primary focus is on who is older.
+2. Ignore surname inheritance issues. For instance, Andrew Williams could still be the biological father of Christopher Baker. We only care about who is older and do not need to consider whether a child should inherit the father's or mother's surname.
+3. At the end of your response, remember to put your final answer within \\boxed{{}}. For example: "So the oldest relative '{last_person}' can trace back to in the provided text is \\boxed{{(your answer here)}}."
+
+'''
+                elif quesiton_position == 'Start':
+                    prompt = f'''This is a test of long-text capability. You need to first read the question below, and then answer it based on the information in the long document that follows.
+Now please answer: {retrieval_question}
+
+For example:
+Example 1: If James Hill's father is Jasmine Lane, and no further information about familial relationships is provided in the text, then the oldest relative James Hill can trace back to in the provided text is \\boxed{{Jasmine Lane}}.
+Example 2: If Andrew Williams's grandmother is Dan Newton, and Dan Newton's father is James Hill, and no further information about familial relationships is provided in the text, then the oldest relative Andrew Williams can trace back to in the provided text is \\boxed{{James Hill}}.
+Example 3: If Jeff White's father is Kevin Le, Dan Newton's grandmother is Jeff White, and Jeff White's father is Kevin Le, and Shelley Mills is Dan Newton's great-granddaughter, and no further information about familial relationships is provided in the text, then the oldest relative Shelley Mills can trace back to in the provided text is \\boxed{{Kevin Le}}.
+
+Notes:
+1. You do not need to worry about the gender consistency of names in this test. For example, a name that is typically considered feminine can still be the father of another person. Our primary focus is on who is older.
+2. Ignore surname inheritance issues. For instance, Andrew Williams could still be the biological father of Christopher Baker. We only care about who is older and do not need to consider whether a child should inherit the father's or mother's surname.
+3. At the end of your response, remember to put your final answer within \\boxed{{}}. For example: "So the oldest relative '{last_person}' can trace back to in the provided text is \\boxed{{(your answer here)}}."
+
+The content of the long document is as follows
+
+<Document>
+{context}
+</Document>
+
+'''
                 else:
-                    raise ValueError(f'Unsupported position {position}. '
+                    raise ValueError(f'Unsupported quesiton_position {quesiton_position}. '
                                      'Position must be "End" or "Start".')
             else:
                 raise ValueError(f"Language '{language}' is not supported.")
@@ -243,6 +252,7 @@ class NeedleBenchMultiDataset(BaseDataset):
                 random.shuffle(lines)
                 random_needle_data = get_random_needles(
                     counter, needle_file_path, num_needles+1, language)
+                last_person = random_needle_data['last_person']
                 needles = [
                     '\n' + needle + '\n'
                     for needle in random_needle_data['needles']
@@ -269,66 +279,13 @@ class NeedleBenchMultiDataset(BaseDataset):
                     needles)
 
                 processed_prompt = _generate_prompt(processed_text,
-                                                    retrieval_question)
+                                                    retrieval_question, last_person)
 
                 data['prompt'].append(processed_prompt)
-                data['answer'].append(answer + '*' + keyword)
+                data['answer'].append(keyword)
 
         dataset = Dataset.from_dict({
             'prompt': data['prompt'],
             'answer': data['answer'],
         })
         return dataset
-
-
-class NeedleBenchMultiEvaluator(BaseEvaluator):
-
-    def levenshtein_distance(self, s1, s2):
-        if len(s1) < len(s2):
-            return self.levenshtein_distance(s2, s1)
-
-        if len(s2) == 0:
-            return len(s1)
-
-        previous_row = range(len(s2) + 1)
-        for i, c1 in enumerate(s1):
-            current_row = [i + 1]
-            for j, c2 in enumerate(s2):
-                insertions = previous_row[j + 1] + 1
-                deletions = current_row[j] + 1
-                substitutions = previous_row[j] + (c1 != c2)
-                current_row.append(min(insertions, deletions, substitutions))
-            previous_row = current_row
-
-        return previous_row[-1]
-
-    def score(self, predictions, gold):
-        if len(predictions) != len(gold):
-            return {'error': 'predictions and gold have different lengths'}
-
-        total_score = 0
-        details = []
-
-        for prediction, reference in zip(predictions, gold):
-            answer, keyword = reference.split('*')
-            keywords = keyword.lower().split()
-            prediction = prediction.lower()
-
-            keyword_score = 100 / len(keywords) if keywords else 0
-
-            matched_keywords = sum(1 for kword in keywords
-                                   if kword in prediction)
-            score = matched_keywords * keyword_score
-
-            detail = {
-                'pred': prediction,
-                'answer': reference,
-                'matched_keywords': matched_keywords,
-                'score': score
-            }
-
-            total_score += score
-            details.append(detail)
-
-        average_score = total_score / len(predictions) if predictions else 0
-        return {'score': average_score, 'details': details}
